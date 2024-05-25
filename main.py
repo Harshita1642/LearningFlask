6504efc3ff49f66a427be6faf7a50a08f8cb552e
harshita <harshita19062004@gmail.com>
2024-05-25 19:26:14 +0530

    video-streaming route through flask to html

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-#Integrate HTML With Flask
-#HTTP verb GET and POST
+from flask import Flask,render_template,Response
+import cv2
+
+app=Flask(__name__)
+camera=cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('entry.html')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run(debug=True)
